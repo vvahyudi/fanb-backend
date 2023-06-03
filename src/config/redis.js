3b8c2023489422cc316ee413df0bc92f9e3704bd
Ahmad Wahyudi <ahmadwahyudi2395@gmail.com>
2023-06-03 16:28:03 +0700

    add boilerplate

--- src/config/redis.js
+++ src/config/redis.js
@@ -5,6 +5,8 @@ const client = redis.createClient({
 	url: `redis://${REDIS_USER}:${REDIS_PASSWORD}@${REDIS_HOST}:${REDIS_PORT}`,
 })
 
+/* This is an immediately invoked async function expression (IIFE) that connects to a Redis server
+using the `redis` package. */
 ;(async () => {
 	client.on("connect", () => {
 		console.log("Redis Connected")
